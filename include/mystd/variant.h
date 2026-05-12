2e054ca8d889a24fcd51ce709f0aa798cfc54fb8
wang <wangminhong73@gmail.com>
2026-05-12 22:54:55 +0800

    Fix visitor pattern in variant, also use decay_t to strip ref

--- include/mystd/variant.h
+++ include/mystd/variant.h
@@ -1,33 +1,35 @@
+#include <algorithm>
 #include <cstddef>
 #include <utility>
 
 namespace mystd {
 template <typename... Args>
 class variant {
-  public:
+public:
     variant() : index_(0) {}
     template <typename T>
     variant(T &&item) {
-        new (field_) T(std::forward<T>(item));
-        index_ = index_of<T, Args...>();
+        using U = std::decay_t<T>;
+        new (field_) U(std::forward<T>(item));
+        index_ = index_of<U, Args...>();
     };
     variant(const variant &other) : index_(other.index_) {
         if (index_ == 0)
             return;
-        using Fn_copy_t = std::size_t (*)(const void *);
-        static Fn_copy_t table[]{[field_ = field_](const void *outer) {
-            new (field_) Args(*reinterpret_cast<const Args *>(outer));
+        using Fn_copy_t = void *(*)(void *, const void *);
+        static Fn_copy_t table[]{[](void *field, const void *outer) {
+            new (field) Args(*reinterpret_cast<const Args *>(outer));
         }...};
-        table[index_ - 1](other.field_);
+        table[index_ - 1](field_, other.field_);
     }
     variant(variant &&other) : index_(other.index_) {
         if (index_ == 0)
             return;
-        using Fn_copy_t = std::size_t (*)(void *);
-        static Fn_copy_t table[]{[field_ = field_](void *outer) {
-            new (field_) Args(std::move(*reinterpret_cast<Args *>(outer)));
+        using Fn_copy_t = void *(*)(void *, void *);
+        static Fn_copy_t table[]{[](void *field, void *outer) {
+            new (field) Args(std::move(*reinterpret_cast<Args *>(outer)));
         }...};
-        table[index_ - 1](other.field_);
+        table[index_ - 1](field_, other.field_);
         other.index_ = 0;
     }
     variant &operator=(const variant &other) {
@@ -36,11 +38,11 @@ class variant {
         index_ = other.index_;
         if (index_ == 0)
             return;
-        using Fn_copy_t = std::size_t (*)(const void *);
-        static Fn_copy_t table[]{[field_ = field_](const void *outer) {
-            new (field_) Args(*reinterpret_cast<const Args *>(outer));
+        using Fn_copy_t = void *(*)(const void *);
+        static Fn_copy_t table[]{[](void *field, const void *outer) {
+            new (field) Args(*reinterpret_cast<const Args *>(outer));
         }...};
-        table[index_ - 1](other.field_);
+        table[index_ - 1](field_, other.field_);
         return *this;
     }
     variant &operator=(variant &&other) {
@@ -49,11 +51,11 @@ class variant {
         index_ = other.index_;
         if (index_ == 0)
             return;
-        using Fn_copy_t = std::size_t (*)(void *);
-        static Fn_copy_t table[]{[field_ = field_](void *outer) {
-            new (field_) Args(std::move(*reinterpret_cast<Args *>(outer)));
+        using Fn_copy_t = void *(*)(void *, void *);
+        static Fn_copy_t table[]{[](void *field, void *outer) {
+            new (field) Args(std::move(*reinterpret_cast<Args *>(outer)));
         }...};
-        table[index_ - 1](other.field_);
+        table[index_ - 1](field_, other.field_);
         other.index_ = 0;
         return *this;
     }
@@ -61,20 +63,22 @@ class variant {
     // used to pass a callable to use field_ value
     template <typename Visitor>
     void visit(Visitor &&v) {
-        using Fn = void (*)(void *);
-        static Fn table[]{[field_ = field_](void *callable) {
-            callable(*reinterpret_cast<Args *>(field_));
+        using Fn = void (*)(void *, void *);
+        static Fn table[]{[](void *field, void *callable) {
+            (*reinterpret_cast<Visitor *>(callable))(
+                *reinterpret_cast<Args *>(field));
         }...};
-        table[index_ - 1](v);
+        table[index_ - 1](field_, &v);
     }
     ~variant() {
-        using Fn = void (*)();
-        static Fn table[]{[field_ = field_]() { field_->~Args(); }...};
-        table[index_ - 1]();
+        using Fn = void (*)(void *);
+        static Fn table[]{
+            [](void *field) { reinterpret_cast<Args *>(field)->~Args(); }...};
+        table[index_ - 1](field_);
     }
 
-  private:
-    alignas(Args...) std::byte field_[max({sizeof(Args)...})];
+private:
+    alignas(Args...) std::byte field_[std::max({sizeof(Args)...})];
     std::size_t index_;
     template <typename T, typename First, typename... Rest>
     constexpr std::size_t index_of() {
